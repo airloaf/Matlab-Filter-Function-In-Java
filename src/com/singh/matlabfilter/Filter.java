b0d795d86ab082b8e86bd6edcc9838f62091d53b
airloaf <vikzilla18@gmail.com>
2017-07-07 20:14:20 -0400

    Implemented Filter function
    HAVEN'T TEST YET

--- src/com/singh/matlabfilter/Filter.java
+++ src/com/singh/matlabfilter/Filter.java
@@ -1,5 +1,49 @@
 package com.singh.matlabfilter;
 
+import java.math.BigDecimal;
+
 public class Filter {
 
+	public static void main(String[] args){
+		
+		
+		
+	}
+	
+	public BigDecimal[] filter(BigDecimal[] b, BigDecimal[] a, BigDecimal[] x){
+		
+		int n = b.length;
+		
+		BigDecimal[] z = new BigDecimal[n];
+		for(int i = 0; i < z.length; i++)
+			z[i] = BigDecimal.ZERO;
+		
+		for(int i = 0; i < b.length; i++){
+			b[i] = b[i].divide(a[0]);
+		}
+		
+		for(int i = 0; i < a.length; i++){
+			a[i] = a[i].divide(a[0]);
+		}
+		
+		BigDecimal[] Y = new BigDecimal[x.length];
+		for(int i = 0; i < Y.length; i++)
+			Y[i] = BigDecimal.ZERO;
+		
+		for(int m = 0; m < Y.length; m++){
+			
+			Y[m] = b[0].multiply(x[m]).add(z[0]);
+			
+			for(int i= 1; i < n; i++){
+				
+				z[i-1] = b[i].multiply(x[m]).add(z[i]).subtract(a[i].multiply(Y[m]));
+				
+			}
+			
+		}
+		
+		return Y;
+		
+	}
+	
 }
